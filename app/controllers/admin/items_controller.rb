af538e94e18b8b43a6dc6b70c571a2b35bc77be8
Daiki Umezawa <137897494+umejaki@users.noreply.github.com>
2023-09-16 06:04:17 +0000

    add image

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,7 +6,7 @@ class Admin::ItemsController < ApplicationController
    def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to admin_item_path
+    redirect_to admin_items_path
    end
    
    def index
@@ -24,7 +24,7 @@ class Admin::ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     @item.update(item_params)
-    redirect_to admin_item_path(@item.id)
+    redirect_to admin_items_path(@item.id)
   end
   
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  belongs_to :admin
+  
+  def get_image(width, height)
+    unless image.attached?
+    file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+  
 end
