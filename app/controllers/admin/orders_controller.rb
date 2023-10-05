4b3a752c0f398dea2baef7086ac2ea206f414824
Daiki Umezawa <137897494+umejaki@users.noreply.github.com>
2023-10-05 11:47:51 +0000

    order show coplete

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,10 @@
 class Admin::OrdersController < ApplicationController
 
   def show
-    @customer = Customer.find(params[:id]) 
     @order = Order.find(params[:id])
-    @order_product = @OrderProduct.find(params[:id])
+    @customer = @order.customer
+    @order_products = @order.order_products
+    @order_product = OrderProduct.find(@order.id)
   end 
 
 end
